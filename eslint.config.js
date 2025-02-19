b7a0f2ea4c33bb7c6e89b553fb38472bf83c0f83
Kevin Poorman <kjp@codefriar.com>
2025-02-19 11:09:08 -0800

    fix(eslint): Additional eslint config
    to ignore dist/ and node_modules/
    Additionally to add require and console to the globals.

--- eslint.config.js
+++ eslint.config.js
@@ -2,16 +2,25 @@ import eslint from '@eslint/js';
 import tseslint from '@typescript-eslint/eslint-plugin';
 import typescript from '@typescript-eslint/parser';
 import prettier from 'eslint-config-prettier';
+import globals from 'globals';
 
 export default [
     eslint.configs.recommended,
+    {
+        ignores: ['**/dist/**/*.+(js|ts)', '**/node_modules/**/*.+(js|ts)'],
+    },
     {
         files: ['**/*.ts', '**/*.tsx'],
         languageOptions: {
             parser: typescript,
             parserOptions: {
                 ecmaVersion: 'latest',
-                sourceType: 'module'
+                sourceType: 'module',
+
+            },
+            globals: {
+                ...globals.browser,
+                ...globals.node
             }
         },
         plugins: {
