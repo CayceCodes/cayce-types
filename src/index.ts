ad485f983c60ea399737f3e96d12f538c419c57b
Kevin Poorman <kjp@codefriar.com>
2025-02-20 12:10:46 -0800

    chore(source-fragment): removes source fragment

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,10 @@
 import ScanResult from './scan-result.js';
-import SourceFragment from './source-fragment.js';
 import { ScanRule, context, message, name, priority, query, regex, suggestion, category } from './scan-rule.js';
 import { ResultType } from './scan-result.js';
 import { SourceLoupePlugin } from './sourceloupe-plugin.js';
 
 export {
     ScanResult,
-    SourceFragment,
     ScanRule,
     SourceLoupePlugin,
     ResultType,
