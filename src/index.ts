4a4770138e880b75c44e7d7158807fa1d02d34cb
Kevin Poorman <kjp@codefriar.com>
2025-03-05 11:41:35 -0800

    refactor(rules): changing rule definitions
    Pushing the work into the rules, rather than scan manager, etc.
    
    BREAKING CHANGE: new rule definitions and shape.

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import ScanResult from './scan-result.js';
 import { ScanRule, context, message, name, suggestion, category, treeQuery, ruleSeverity } from './scan-rule.js';
-import RuleSeverity from './scan-result.js';
+import { RuleSeverity } from './rule-severity.js';
 import { CaycePlugin, CayceBasePlugin } from './cayce-plugin.js';
 import { Formatter, OutputFormat } from './formatter.js';
 import { CsvFormatter } from './formatters/csv-formatter.js';
